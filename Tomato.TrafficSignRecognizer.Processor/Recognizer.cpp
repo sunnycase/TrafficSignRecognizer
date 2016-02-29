e2b9583164227ca35e74973a31fb19008abef5b1
sunnycase <sunnycase@live.cn>
2016-02-29 14:05:07 +0800

    利用SUSAN算法实现边缘查找

--- Tomato.TrafficSignRecognizer.Processor/Recognizer.cpp
+++ Tomato.TrafficSignRecognizer.Processor/Recognizer.cpp
@@ -78,15 +78,52 @@ concurrency::task<void> Recognizer::PrepareTargetImage(BitmapFrame ^ frame)
 
 concurrency::task<void> Recognizer::FindContours()
 {
-	auto& inputTex(_targetImage);
+	uint radius = 3;
+
+	int same = 0, total = 0;
+	// 圆扫描线 x = n
+	auto maxY = (int)radius;
+	for (int y = -maxY; y <= maxY; y++)
+	{
+		float x² = radius * radius - y * y;
+		float x1 = fast_math::sqrt(x²);
+		auto x2 = -x1;
+
+		for (float x = x2; x <= x1; x += 1.f)
+			total++;
+	}
+
+	texture_view<const unorm_4, 2> inputTex(_targetImage);
 	array_view<uint, 2> outputTex(_outputTex);
-	parallel_for_each(inputTex.extent, [&inputTex, outputTex](index<2> idx) restrict(amp)
+	parallel_for_each(inputTex.extent, [inputTex, outputTex, radius, total](index<2> idx) restrict(amp)
 	{
 		auto pixel = inputTex[idx];
 		// 转化为灰度图
-		auto gray = Grayscale(pixel);
+		float gray = Grayscale(pixel);
 
-		auto lastGray = uint(gray * 255);
+		float sum = 0.f;
+		int same = 0;
+		// 圆扫描线 x = n
+		auto maxY = (int)radius;
+		for (int y = -maxY; y <= maxY; y++)
+		{
+			const auto x² = float(radius * radius - y * y);
+			const auto x1 = fast_math::sqrt(x²);
+			const auto x2 = -x1;
+
+			const auto coordY = float(idx[0] + y) / (float)inputTex.extent[0];
+			for (float x = x2; x <= x1; x += 1.f)
+			{
+				const auto coordX = float(idx[1] + x) / (float)inputTex.extent[1];
+				float theGray = Grayscale(inputTex.sample(float_2(coordX, coordY)));
+				sum += theGray;
+				auto threshold = 0.1f;
+				if (fast_math::fabs(theGray - gray) <= threshold)
+					same++;
+			}
+		}
+
+		auto lastGray = same < (total / 2) ? 255 : 0;
 		outputTex[idx] = uint(0xFF000000 | (lastGray << 16) | (lastGray << 8) | lastGray);
 	});
 	return task_from_result();
